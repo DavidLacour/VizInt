ff77de9f4214dd9dbe31a6f884d9e7ff6379eaa5
DavidLacour <davidlacourgaletto@gmail.com>
2025-05-23 19:00:22 +0200

    conf

---
+++ autoflex/autotinymodels/submit2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=autobaselines         # Change as needed
+#SBATCH --time=6:00:00
+#SBATCH --account=cs-503
+#SBATCH --qos=cs-503
+#SBATCH --gres=gpu:1                    # Request 2 GPUs
+#SBATCH --mem=64G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10               # Adjust CPU allocation if needed
+#SBATCH --output=interactive_job_autobaselines.out    # Output log file
+#SBATCH --error=interactive_job_autobaselines.err     # Error log file
+
+CONFIG_FILE=$1
+WANDB=$2
+NUM_GPUS=$3
+
+
+~/miniconda3/bin/conda init bash
+source ~/.bashrc
+conda activate nanofm
+export WANDB_ENTITY=david-lacour-epfl
+export WANDB_API_KEY=1d6641b737cd13fe32a9371dd3780308fee23512 
+python train_baselines.py --dataset ../tiny-imagenet-200 --epochs 50
