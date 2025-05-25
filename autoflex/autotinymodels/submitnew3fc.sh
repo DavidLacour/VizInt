e7e51fa7333211a12d89fad49529f437a69e653f
DavidLacour <davidlacourgaletto@gmail.com>
2025-05-25 22:36:41 +0200

    config

--- autoflex/autotinymodels/submitnew3fc.sh
+++ autoflex/autotinymodels/submitnew3fc.sh
@@ -20,4 +20,4 @@ conda activate nanofm
 
 export WANDB_ENTITY=david-lacour-epfl
 export WANDB_API_KEY=1d6641b737cd13fe32a9371dd3780308fee23512 
-python main_baselines_3fc_integration.py --train_all  --dataset ../../../tinyimagenet200 --model_dir ../../newnotint3fc
+python main_baselines_3fc.py --train_all  --dataset ../../../tinyimagenet200 --model_dir ../../newnotint3fc
