6a4f6cef95756ba1d1940fae618583caa30798d8
DavidLacour <davidlacourgaletto@gmail.com>
2025-06-06 13:10:34 +0200

    slurm job settings

--- rendu/src/submittinyimagenetrendu.sh
+++ rendu/src/submittinyimagenetrendu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=tiny_funky5      # Change as needed
-#SBATCH --time=48:00:00
+#SBATCH --job-name=tiny_newLoss     # Change as needed
+#SBATCH --time=24:00:00
 #SBATCH --account=cs-503
 #SBATCH --qos=cs-503
 #SBATCH --gres=gpu:1                    # Request 2 GPUs
 #SBATCH --mem=64G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5               # Adjust CPU allocation if needed
-#SBATCH --output=interactive_job_tiny_funky5.out    # Output log file
-#SBATCH --error=interactive_job_tiny_funky5.err     # Error log file
+#SBATCH --output=interactive_job_tiny_newLoss.out    # Output log file
+#SBATCH --error=interactive_job_tiny_newLoss.err     # Error log file
 
 CONFIG_FILE=$1
 WANDB=$2
